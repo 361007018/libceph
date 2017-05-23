1a4b41727b2a142109161e351710a38f946585c3
Viva La Vida <wujie@gzsunrun.cn>
2017-05-23 14:47:26 +0800

    add function stat of osd

--- actions/osd.go
+++ actions/osd.go
@@ -1,1 +1,24 @@
 package actions
+
+import (
+	"encoding/json"
+)
+
+type Osd struct {
+	ActionBase
+}
+
+func (this *Osd) Stat() ([]byte, error) {
+	cmdline, err := json.Marshal(map[string]interface{}{
+		"prefix": "osd stat",
+		"format": "json",
+	})
+	if err != nil {
+		return nil, err
+	}
+	result, _, err := this.CephConn.Rados.MonCommand(cmdline)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
