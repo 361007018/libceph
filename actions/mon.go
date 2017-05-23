04c6a8955ac24ac807c3b181131333a5cc15534a
Viva La Vida <wujie@gzsunrun.cn>
2017-05-23 14:46:53 +0800

    add function status of mon

--- actions/mon.go
+++ actions/mon.go
@@ -1,1 +1,25 @@
 package actions
+
+import (
+	"encoding/json"
+)
+
+// Cluster ...
+type Mon struct {
+	ActionBase
+}
+
+// Status ...
+func (this *Mon) Status() ([]byte, error) {
+	cmdline, err := json.Marshal(map[string]interface{}{
+		"prefix": "mon_status",
+	})
+	if err != nil {
+		return nil, err
+	}
+	result, _, err := this.CephConn.Rados.MonCommand(cmdline)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
