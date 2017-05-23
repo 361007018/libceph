e85f8f8897a4378a7bafca1c0aa6f6d40074ab9f
Viva La Vida <wujie@gzsunrun.cn>
2017-05-23 16:31:54 +0800

    add function quorumstatus in cluster

--- actions/cluster.go
+++ actions/cluster.go
@@ -4,12 +4,24 @@ import (
 	"encoding/json"
 )
 
-// Cluster ...
 type Cluster struct {
 	ActionBase
 }
 
-// Status ...
+func (this *Cluster) QuorumStatus() ([]byte, error) {
+	cmdline, err := json.Marshal(map[string]interface{}{
+		"prefix": "quorum_status",
+	})
+	if err != nil {
+		return nil, err
+	}
+	result, _, err := this.CephConn.Rados.MonCommand(cmdline)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
 func (this *Cluster) Status() ([]byte, error) {
 	cmdline, err := json.Marshal(map[string]interface{}{
 		"prefix": "status",
